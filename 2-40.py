38c9fe8c1389a45c6d09e7f2d8d23bb55b2a9ade
CHANN <hxperl@gmail.com>
2018-08-06 20:48:41 +0900

    a

---
+++ 2-40.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from datetime import datetime
+
+class dateKoreanType:
+    def __init__(self, format):
+        self.format = format
+    
+    def __call__(self, *args, **kwargs):
+        return datetime.strptime(args[0], self.format)
+
+@app.route("/board", methods=["GET", "POST"])
+def board():
+    print(request.values.get("date", "2015-02-09", type=dateKoreanType("%Y-%m-%d")))
+    return "check ur console"
