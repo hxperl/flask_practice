c2e155e592794726e640c45958e312d666d84f87
CHANN <hxperl@gmail.com>
2018-08-07 13:00:30 +0900

    a

---
+++ 2-60.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/example/json", methods=["POST"])
+def example_json():
+    print(request.get_json())
+    return ""
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
