6aedf2e6e5a214b54c4438aaf72bbc6cae77fb37
CHANN <hxperl@gmail.com>
2018-08-07 13:17:02 +0900

    a

--- 2-61.py
+++ 2-61.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+app.config.update(MAX_CONTENT_LENGTH=1024*1024*10)
 
 @app.route("/example/max_content_len", methods=["GET"])
 def example_content_length():
