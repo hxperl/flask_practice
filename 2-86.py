a0c509eb2eb55848049ad3fea6469afc40888ef5
CHANN <hxperl@gmail.com>
2018-08-21 09:53:10 +0900

    a

---
+++ 2-86.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+app.config.update(DEBUG=True)
+
+file_log_format = logging.Formatter('%(levelname)-8s %(message)s')
+
+file_logger = logging.FileHandler("flask_instance.log")
+
+file_logger.setFormatter(file_logger)
+
+app.logger.addHandler(file_logger)
+
+@app.route("/log")
+def logger():
+    app.loger.debug("print DEBUG message")
+    return "check console"
+
+app.run(host="0.0.0.0")
